a8ad4cd0c1badc51505916f6787050a829b546a5
sung <777282alswjd@gmail.com>
2023-06-10 15:49:20 +0900

    [#49] style : Detail 페이지 텍스트 추가

--- src/pages/Detail/MissionDetail.jsx
+++ src/pages/Detail/MissionDetail.jsx
@@ -16,7 +16,7 @@ import JoinSticky from '../../components/Join/JoinSticky';
 import img from '../../assets/MissionImg/Running.png';
 
 function MissionDetail() {
- const detailDate =   {
+  const detailDate = {
     type: '운동', // 주제
     title: '마라톤 같이 뛰어요!', // 제목
     recruitmentPeriod: '2023년 06월 01일까지', // 모집기간
@@ -26,7 +26,7 @@ function MissionDetail() {
     participant: 100, // 미션참가자 수
     rule: '<p>테스트</p><p><br></p><p><strong>이건 테스트 입니다</strong></p><h6>test test test</h6><p>1234</p><p><br></p><p>하나 둘 셋 넷</p>', // 상세 내용
   };
-  
+
   return (
     <div>
       <$titleDiv>
@@ -49,20 +49,28 @@ function MissionDetail() {
                 </$Table2>
               </div>
               <div>
-                <$Table1 style={{borderBottom : '1px solid #999999'}}>
+                <$Table1 style={{ borderBottom: '1px solid #999999' }}>
                   <$TableText1>미션기간</$TableText1>
                 </$Table1>
-                <$Table2 style={{borderBottom : '1px solid #999999'}}>
+                <$Table2 style={{ borderBottom: '1px solid #999999' }}>
                   <$TableText2>{detailDate.missionPeriod}</$TableText2>
                 </$Table2>
               </div>
-              <$Line style={ {paddingTop : '50px',width:'655px', color :'#BABABA'}}></$Line>
+              <$Line
+                style={{ paddingTop: '50px', width: '655px', color: '#BABABA' }}
+              ></$Line>
+              <div style={{ marginTop: '50px' }}>
+                {React.createElement('div', {
+                  dangerouslySetInnerHTML: { __html: detailDate.rule },
+                })}
+              </div>
             </div>
           </$LeftDiv>
-          <JoinSticky/>
+          <JoinSticky />
         </$TotalDiv>
       </div>
-</div>
-)};
+    </div>
+  );
+}
 
 export default MissionDetail;
